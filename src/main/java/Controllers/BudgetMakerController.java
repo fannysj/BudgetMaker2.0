ce7719f450fa59146718c6eacf03fc264ed55ce7
nadinelindenmalmberg <nadine.linden.malmberg@gmail.com>
2022-09-08 14:31:42 +0200

    fix design

--- src/main/java/Controllers/BudgetMakerController.java
+++ src/main/java/Controllers/BudgetMakerController.java
@@ -4,9 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.fxml.Initializable;
-//import java.util.ResourceBundle;
-//import java.net.URL;
 
 public class BudgetMakerController {
 
