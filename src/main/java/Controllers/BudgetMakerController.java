4c04feb9bcdfffabc03a7c3230d0969d08107c2d
oscarsavinger <o.savinger@gmail.com>
2022-09-14 13:42:20 +0200

    balance to an int

--- src/main/java/Controllers/BudgetMakerController.java
+++ src/main/java/Controllers/BudgetMakerController.java
@@ -14,6 +14,32 @@ public class BudgetMakerController {
     @FXML
     private Button Budgetknapp;
 
+    @FXML
+    private AnchorPane startSida;
+
+    @FXML
+    private AnchorPane testSida;
+
+    @FXML
+    private Label budgetAmount;
+
+    @FXML
+    public void bytSida() {
+        testSida.toFront();
+        startSida.setVisible(false);
+        testSida.setVisible(true);
+
+        budgetAmount.setText(EnterBudget.getText());
+        int x = getIntFromTextField(EnterBudget);
+        budgetAmount.setText(Integer.toString(x));
 
 
+    }
+
+
+    public static int getIntFromTextField(TextField EnterBudget) {
+        String text = EnterBudget.getText();
+        return Integer.parseInt(text);
+
+    }
 }
