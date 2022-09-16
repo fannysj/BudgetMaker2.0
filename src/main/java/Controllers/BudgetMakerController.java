1b77eb1558c6066eeda569afca30764f321eb942
fannysj <fannysj@chalmers.se>
2022-09-16 17:29:19 +0200

    fix av categorylistitem

--- src/main/java/Controllers/BudgetMakerController.java
+++ src/main/java/Controllers/BudgetMakerController.java
@@ -4,6 +4,7 @@ import Model.newBudget;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
