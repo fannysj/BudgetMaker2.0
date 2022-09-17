5a2f31ca99e3cf2bd7852b2c4cb1e61319967326
fannysj <fannysj@chalmers.se>
2022-09-17 10:17:49 +0200

    categorylistitem

--- src/main/java/Controllers/BudgetMakerController.java
+++ src/main/java/Controllers/BudgetMakerController.java
@@ -1,15 +1,22 @@
 package Controllers;
 
+import Model.Category;
+import Model.CategoryListItem;
 import Model.newBudget;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
 
-public class BudgetMakerController {
-    newBudget budget;
+import java.util.*;
+import java.net.URL;
+
+
+public class BudgetMakerController implements Initializable {
+    private List<String> catelist = new ArrayList<>(Arrays.asList("mat","shopping"));
+    private Map<Label, CategoryListItem> CategoryListItemMap = new HashMap<Label, CategoryListItem>();
+
 
     @FXML
     private TextField EnterBudget;
@@ -30,6 +37,9 @@ public class BudgetMakerController {
     private Button goBack;
 
     @FXML
+    private FlowPane categorylist;
+
+    @FXML
     private Button backToOverview;
 
     @FXML
@@ -43,6 +53,17 @@ public class BudgetMakerController {
 
     @FXML
     private AnchorPane addExpenseAnchorPane;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+        for (String cate : catelist){
+            categorylist.getChildren().add(new CategoryListItem(cate, this));
+
+        }
+    }
 
     @FXML
     public void setAddExpense(){
@@ -66,6 +87,7 @@ public class BudgetMakerController {
         testSida.toFront();
         startSida.setVisible(false);
         testSida.setVisible(true);
+        populatecategories("Mat",1000);
 
         new newBudget(EnterBudget, budgetAmount);
 
@@ -94,6 +116,25 @@ public class BudgetMakerController {
     public void budgetButtonPressed(){
         Budgetknapp.setStyle("-fx-background-color: #6a66a8; -fx-background-radius: 20px; -fx-border-color: #fff; -fx-border-radius: 20px;");
     }
+
+    public void populatecategories(String name, int amount){
+        new Category(name,amount);
+    }
+
+//    public void updateCategoryList(){
+//        categorylist.getChildren().clear();
+//
+//        for(int i =0; i<catelist.size(); i++){
+//            CategoryListItem listItem = (CategoryListItem) catelist.listIterator();
+//            categorylist.getChildren().add(listItem);
+//
+//        }
+//
+//    }
+
+
+
+
 
 
 }
