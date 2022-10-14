359942ef39b954c079631f4804ab1cae4fcec555
larssonmaja23 <larssonmaja23@gmail.com>
2022-10-14 11:09:12 +0200

    Nya tester

--- Tester/CategoryTest.java
+++ Tester/CategoryTest.java
@@ -1,4 +1,5 @@
 import Model.Category;
+import Model.Transaction;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -10,45 +11,45 @@ public class CategoryTest {
 
     Category c = new Category("Kategori", 1000);
     LocalDate d = LocalDate.now();
+    Transaction tran = c.newTransaction(100, "Willys", "Korv", d);
+
+
+    @Test //set.SpentAmount körs över av get.SpentAmount
+    public void testingSetterAndGetter() {
+        Category original = new Category("Ostbågar", 1000);
+        Category copy = new Category("Ostbågar", 1000);
+
+        copy.setGoalAmount(500);
+        int cga = copy.getGoalAmount();
+        int oga = original.getGoalAmount();
+
+        copy.setName("Kopia");
+        String cn = copy.getName();
+        String on = original.getName();
+
+        copy.setSpentAmount(20);
+
+        assertNotEquals(cga, oga);
+        assertNotEquals(cn, on);
+        assertTrue(copy.spentAmount == 20);
+    }
+
 
     @Test
-    public void transactionsShouldBeAddedToTransactionList(){
-        c.newTransaction(100, "HM", "Vinterjacka", d);
-        c.newTransaction(50, "Zara", "Skor", d);
-        assertTrue(c.getTransactionsList().size() == 2);
+    public void CreateTransactionAndAddItToList() {
+        Transaction t = c.newTransaction(300, "Shopping", "Jacka", d);
+        c.addTransactionToList(t);
+        assertTrue(c.transactionsList.size() == 1);
 
     }
 
     @Test
-    public void addingTransactionsShouldChangeSpentAmountInCategory(){
-        c.newTransaction(100, "HM", "Vinterjacka", d);
-        c.newTransaction(20, "Zara", "Skor", d);
+    public void updateSpentAmountBasedOnAddedTransactions(){
+        c.addTransactionToList(tran);
         c.updateSpentAmount();
-        assertTrue(c.getSpentAmount() == 120);
+        assertEquals(c.spentAmount,tran.getTransactionAmount());
     }
 
-    @Test
-   public void addingTransactionsShouldChangeAmountLeftToSpendInCategory(){
-        c.newTransaction(100, "HM", "Vinterjacka", d);
-        double a = c.AmountLeftToSpend();
-        assertTrue(a == 900 );
-
-    }
-
-    @Test
-    public void YouShouldBeAbleToDeleteATransaction(){
-        c.newTransaction(100, "HM", "Vinterjacka", d);
-        c.newTransaction(20, "Zara", "Skor", d);
-        c.newTransaction(100, "Stadium", "Träningskläder", d);
-        c.deleteTransactionFromList();
-        assertTrue(c.getTransactionsList().size() == 2);
-    }
-
-    @Test //Fungerar ej, vet inte om jag missuppfattar hur setters fungerar?
-    public void settersShouldChangeCategoryInfo(){
-        Category copy = c;
-        copy.setName("Alkohol");
-        copy.setGoalAmount(2000);
-        assertNotEquals(copy, c);
-    }
 }
+
+
